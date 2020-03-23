1fbdfadbc9a9e9d97e4c44195c40537cd13d7111
dammy_ololade <damola@kobo360.com>
2020-03-23 22:28:15 +0100

    set up project

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import {sequelize} from "./sequelize";
+import { createServer } from "http";
+import bodyParser = require("body-parser");
+
+const app = express();
+const port = 8000;
+
+
+// middleware for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// middleware for json body parsing
+app.use(bodyParser.json({limit: "5mb"}));
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the landing page of Cake Villa!!! ');
+});
+
+(async () => {
+    try {
+        await sequelize.authenticate();
+        console.info("Sequelize is Up and Runing.. Move on....");
+    } catch (e) {
+        console.error("Sequelize failed to start up", e);
+    }
+
+    createServer(app).listen(port, () => {
+        return console.info(`server started at http://localhost:${port}`);
+    });
+})();
